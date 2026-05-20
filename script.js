40230e0ccf34cc443043cf8230a7d41f2b19a663
nahubustovaras <nahubustovaras@gmail.com>
2026-05-20 18:00:38 -0300

    Trabajo Finalizado Nahuel

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ alert("Bienvenido a la Tienda Gamer");
 let colorOscuro = true;
 
 function cambiarColor() {
-if (colorOscuro) {
-document.body.style.backgroundColor = "#444";
-} else {
-document.body.style.backgroundColor = "#111";
-}
-colorOscuro = !colorOscuro;
+    if (colorOscuro) {
+        document.body.style.backgroundColor = "#444";
+    } else {
+        document.body.style.backgroundColor = "#111";
+    }
+    colorOscuro = !colorOscuro;
 
-console.log("Color cambiado");
+    console.log("Color cambiado");
 }
 
 function mostrarMensaje() {
-alert("Nuevos productos disponibles!");
+    alert("Nuevos productos disponibles!");
 }
+
+/* FORMULARIO */
+document.getElementById("formulario").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Formulario enviado correctamente");
+});
