3ea77ecdfb78a4cdd21238cb411f5d7817e3f4ed
claudiook-crypto <claucrettini@gmail.com>
2026-05-20 17:46:40 -0300

    Proyecto finalizado

---
+++ script.js
@@ -0,0 +1,24 @@
+alert("Bienvenido a la Tienda Gamer");
+
+let colorOscuro = true;
+
+function cambiarColor() {
+    if (colorOscuro) {
+        document.body.style.backgroundColor = "#444";
+    } else {
+        document.body.style.backgroundColor = "#111";
+    }
+    colorOscuro = !colorOscuro;
+
+    console.log("Color cambiado");
+}
+
+function mostrarMensaje() {
+    alert("Nuevos productos disponibles!");
+}
+
+/* FORMULARIO */
+document.getElementById("formulario").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Formulario enviado correctamente");
+});
